2ee6e9a10cec8d6310fbd6ecef8ef3603a052872
Alex Uskov <FrozenAlex@users.noreply.github.com>
2022-11-25 04:02:57 +0400

    Update scripts

--- createqmod.ps1
+++ createqmod.ps1
@@ -1,16 +1,55 @@
 Param(
-    [String]$qmodname="",
+    [String] $qmodname="",
+
     [Parameter(Mandatory=$false)]
-    [Switch]$clean
+    [Switch] $clean,
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $help
 )
+
+if ($help -eq $true) {
+    Write-Output "`"BuildQmod <qmodName>`" - Copiles your mod into a `".so`" or a `".a`" library"
+    Write-Output "`n-- Parameters --`n"
+    Write-Output "qmodName `t The file name of your qmod"
+
+    Write-Output "`n-- Arguments --`n"
+
+    Write-Output "-Clean `t`t Performs a clean build on both your library and the qmod"
+
+    exit
+}
 
 if ($qmodName -eq "")
 {
     Write-Output "Give a proper qmod name and try again"
     exit
 }
+
+& $PSScriptRoot/build.ps1 -clean:$clean
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Output "Failed to build, exiting..."
+    exit $LASTEXITCODE
+}
+
+Write-Output "Creating qmod from mod.json"
+
+$schemaUrl = "https://raw.githubusercontent.com/Lauriethefish/QuestPatcher.QMod/main/QuestPatcher.QMod/Resources/qmod.schema.json"
+Invoke-WebRequest $schemaUrl -OutFile ./mod.schema.json
+
 $mod = "./mod.json"
-$modJson = Get-Content $mod -Raw | ConvertFrom-Json
+$schema = "./mod.schema.json"
+$modJsonRaw = Get-Content $mod -Raw
+$modJson = $modJsonRaw | ConvertFrom-Json
+$modSchemaRaw = Get-Content $schema -Raw
+
+Remove-Item ./mod.schema.json
+
+Write-Output "Validating mod.json..."
+if(!($modJsonRaw | Test-Json -Schema $modSchemaRaw)) {
+    exit
+}
 
 $filelist = @($mod)
 
@@ -22,7 +61,7 @@ if ((-not ($cover -eq "./")) -and (Test-Path $cover))
 
 foreach ($mod in $modJson.modFiles)
 {
-        $path = "./build/" + $mod
+    $path = "./build/" + $mod
     if (-not (Test-Path $path))
     {
         $path = "./extern/libs/" + $mod
--- restart-game.ps1
+++ restart-game.ps1
@@ -1,2 +1,5 @@
 adb shell am force-stop com.beatgames.beatsaber
 adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
+# Workaround for Meta piracy notification
+Start-Sleep -Seconds 1.0
+adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
