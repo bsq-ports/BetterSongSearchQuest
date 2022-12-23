79e13492c37ce035af9bbfe3ac8d970daf40e6d7
Alex Uskov <FrozenAlex@users.noreply.github.com>
2022-12-23 12:21:44 +0400

    Add null terminators to bsml assets

--- include/assets.hpp
+++ include/assets.hpp
@@ -1,22 +1,19 @@
 #pragma once
-
 #include <string_view>
 #include "beatsaber-hook/shared/utils/typedefs.h"
-
 struct IncludedAsset {
-
     IncludedAsset(uint8_t* start, uint8_t* end) : array(reinterpret_cast<Array<uint8_t>*>(start)) {
         array->klass = nullptr;
         array->monitor = nullptr;
         array->bounds = nullptr;
-        array->max_length = end - start - 32;
+        array->max_length = end - start - 33;
+        *(end - 1)= '\0';
     }
     
     operator ArrayW<uint8_t>() const {
         init();
         return array;
     }
-
     operator std::string_view() const {
         return { reinterpret_cast<char*>(array->values), array->Length() };
     }
@@ -24,22 +21,17 @@ struct IncludedAsset {
     operator std::span<uint8_t>() const {
         return { array->values, array->Length() };
     }
-
     void init() const {
         if(!array->klass)
             array->klass = classof(Array<uint8_t>*);
     }
-
     private:
         Array<uint8_t>* array;
-
 };
-
 #define DECLARE_FILE(name)                         \
     extern "C" uint8_t _binary_##name##_start[];  \
     extern "C" uint8_t _binary_##name##_end[];    \
     const IncludedAsset name { _binary_##name##_start, _binary_##name##_end};
-
 namespace IncludedAssets {
 
 	DECLARE_FILE(DownloadHistory_bsml)
