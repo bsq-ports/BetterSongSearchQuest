81ee64263c679d4fd99d0c7143bbf330ebe9dc51
Alex Uskov <FrozenAlex@users.noreply.github.com>
2023-01-01 00:36:16 +0400

    Fix date calculations

--- include/DateUtils.hpp
+++ include/DateUtils.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+using namespace std::chrono;
+
 namespace BetterSongSearch {
     int GetMonthsSinceDate(int64_t since)
     {
@@ -8,9 +10,26 @@ namespace BetterSongSearch {
         auto monthsSince = std::chrono::duration_cast<std::chrono::months>(currentEpoch - epoch2);
         return monthsSince.count();
     }
-    auto GetDateAfterMonths(int64_t sinceEpoch, int64_t addMonths)
+    // Add calendar months to a date
+    seconds GetDateAfterMonths(int64_t sinceEpoch, int64_t addMonths)
     {
         std::chrono::seconds tp(sinceEpoch);
-        return tp + std::chrono::months(addMonths);
+
+        // Get timepoint
+        system_clock::time_point timePoint{tp};
+        auto sd = floor<days>(tp);
+        // Record the time of day
+        auto time_of_day = tp - sd;
+        // Convert to a y/m/d calendar data structure
+        year_month_day ymd {(sys_days) sd};
+        // Add the months
+        ymd += months{addMonths};
+        // Add some policy for overflowing the day-of-month if desired
+        if (!ymd.ok())
+            ymd = ymd.year()/ymd.month()/last;
+        // Convert back to system_clock::time_point
+        system_clock::time_point later = sys_days{ymd} + time_of_day;
+
+        return std::chrono::duration_cast<std::chrono::seconds>(later.time_since_epoch());
     }
 }
