f0979b73f2de6cdc79a7ce37df2a1fe7e6a3a212
Alex Uskov <FrozenAlex@users.noreply.github.com>
2023-01-05 06:26:57 +0400

    Covercache fix draft with crashes

--- include/UI/Manager.hpp
+++ include/UI/Manager.hpp
@@ -9,19 +9,30 @@
 #include "UnityEngine/UI/Button_ButtonClickedEvent.hpp"
 #include "UI/FlowCoordinators/BetterSongSearchFlowCoordinator.hpp"
 
+#include <vector>
+
 using namespace QuestUI;
 using namespace BetterSongSearch::UI;
 using namespace BetterSongSearch::Util;
 using namespace GlobalNamespace;
 
 namespace BetterSongSearch::UI {
+
+    struct SongCacheEntry {
+        GlobalNamespace::IBeatmapLevel* level;
+        UnityEngine::Sprite* cover;
+    };
+
     class Manager
     {
         HMUI::FlowCoordinator* parentFlow;
         SafePtrUnity<BetterSongSearch::UI::FlowCoordinators::BetterSongSearchFlowCoordinator> flow;
         BSML::MenuButton * menuButton;
+        
 
         public:
+            std::vector<SongCacheEntry> coverCacheInvalidator;
+            GlobalNamespace::IBeatmapLevel*  lastSelectedLevel = nullptr;
             Manager(Manager const&) = delete; // no accidental copying
             Manager() = default;
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include "questui/shared/QuestUI.hpp"
 #include "System/Action.hpp"
+#include "System/String.hpp"
 #include "custom-types/shared/register.hpp"
 #include "HMUI/FlowCoordinator.hpp"
 #include "HMUI/ViewController_AnimationType.hpp"
@@ -10,17 +11,28 @@
 #include "GlobalNamespace/PlayerDataModel.hpp"
 #include "GlobalNamespace/PlayerData.hpp"
 #include "UnityEngine/GameObject.hpp"
+#include "UnityEngine/UI/Image.hpp"
 #include "GlobalNamespace/PlayerLevelStatsData.hpp"
 #include "GlobalNamespace/IDifficultyBeatmap.hpp"
 #include "GlobalNamespace/SoloFreePlayFlowCoordinator.hpp"
 #include "GlobalNamespace/GameplaySetupViewController.hpp"
 #include "GlobalNamespace/LevelFilteringNavigationController.hpp"
 #include "GlobalNamespace/IBeatmapLevelPack.hpp"
+#include "GlobalNamespace/IBeatmapLevel.hpp"
+#include "GlobalNamespace/StandardLevelDetailView.hpp"
+#include "System/Threading/Tasks/Task_1.hpp"
+#include "System/IO/Path.hpp"
+#include "System/IO/File.hpp"
+
 #include "GlobalNamespace/SongPackMask.hpp"
 #include "GlobalNamespace/SelectLevelCategoryViewController.hpp"
 #include "UI/FlowCoordinators/BetterSongSearchFlowCoordinator.hpp"
 #include "UI/ViewControllers/SongList.hpp"
 #include "PluginConfig.hpp"
+#include "questui/shared/BeatSaberUI.hpp"
+#include "System/Threading/Tasks/TaskCanceledException.hpp"
+
+#include "UnityEngine/Texture2D.hpp"
 #include "UI/Manager.hpp"
 #include "HMUI/TextSegmentedControlCell.hpp"
 #include "bsml/shared/Helpers/delegates.hpp"
@@ -32,7 +44,7 @@
 #define coro(coroutine) GlobalNamespace::SharedCoroutineStarter::get_instance()->StartCoroutine(custom_types::Helpers::CoroutineHelper::New(coroutine))
 
 using namespace QuestUI;
-using namespace BetterSongSearch::Util;
+using namespace GlobalNamespace;
 
 static ModInfo modInfo; // Stores the ID and version of our mod, and is sent to the modloader upon startup
 
@@ -53,6 +65,95 @@ Paper::ConstLoggerContext<17UL> getLogger() {
     static auto fastContext = Paper::Logger::WithContext<MOD_ID>();
     return fastContext;
 }
+
+MAKE_HOOK_MATCH(StandardLevelDetailView_SetContent, &StandardLevelDetailView::SetContent, void, StandardLevelDetailView* self, IBeatmapLevel* level, BeatmapDifficulty defaultDifficulty, BeatmapCharacteristicSO* defaultBeatmapCharacteristic, PlayerData* playerData) {
+    // Prefix
+    // fix
+    StandardLevelDetailView_SetContent(self, level, defaultDifficulty, defaultBeatmapCharacteristic, playerData);
+    // postfix
+    manager.lastSelectedLevel = level;
+};
+
+MAKE_HOOK_MATCH(CustomPreviewBeatmapLevel_GetCoverImageAsync, &CustomPreviewBeatmapLevel::GetCoverImageAsync, System::Threading::Tasks::Task_1<UnityEngine::Sprite *>*, CustomPreviewBeatmapLevel* self, System::Threading::CancellationToken cancellationToken) {
+    static int MAX_CACHED_COVERS = 20;
+    if (self->coverImage != nullptr && self->coverImage->m_CachedPtr.m_value != nullptr) {
+        int cachedIndex = -1;
+        // "Refresh" the cover in the cache LIFO
+        for (auto i=0; i< manager.coverCacheInvalidator.size(); i--) {
+            auto level = reinterpret_cast<CustomPreviewBeatmapLevel*>(manager.coverCacheInvalidator[i].level);
+            if (level == self) {
+                cachedIndex = i;
+                break;
+            } 
+        }
+
+        // Move to top
+        if (cachedIndex != 1 && cachedIndex + 1 != manager.coverCacheInvalidator.size()) {
+            manager.coverCacheInvalidator.push_back(manager.coverCacheInvalidator[cachedIndex]);
+            manager.coverCacheInvalidator.erase(manager.coverCacheInvalidator.begin()+cachedIndex);
+        }
+
+        DEBUG("Cached image");
+        return System::Threading::Tasks::Task_1<UnityEngine::Sprite *>::FromResult(self->coverImage);
+    }
+
+    if (System::String::IsNullOrEmpty(self->standardLevelInfoSaveData->coverImageFilename)) {
+        DEBUG("Default image");
+        return System::Threading::Tasks::Task_1<UnityEngine::Sprite *>::FromResult(self->defaultCoverImage);
+    }
+
+    StringW path = System::IO::Path::Combine(self->customLevelPath, self->standardLevelInfoSaveData->coverImageFilename);
+
+    if(!System::IO::File::Exists(path)) {
+        DEBUG("File does not exist");
+        return System::Threading::Tasks::Task_1<UnityEngine::Sprite *>::FromResult(self->defaultCoverImage);
+    }
+
+    // UnityEngine::Sprite* image = UnityEngine::Sprite::
+
+    auto sprite = QuestUI::BeatSaberUI::FileToSprite((std::string) path);
+    // fix
+    if (sprite == nullptr  || sprite->m_CachedPtr.m_value == nullptr) {
+        return System::Threading::Tasks::Task_1<UnityEngine::Sprite *>::FromResult(self->defaultCoverImage);
+    }
+    if (cancellationToken.get_IsCancellationRequested()) {
+        DEBUG("Fix tis");
+        // return System::Threading::Tasks::Task_1<UnityEngine::Sprite *>::FromException(System::Exception::New_ctor("Cancelled"));
+    }
+    self->coverImage = sprite;
+
+    manager.coverCacheInvalidator.push_back({
+        reinterpret_cast<GlobalNamespace::IBeatmapLevel*>(self), self->coverImage
+    });
+
+    for(int i = manager.coverCacheInvalidator.size() - MAX_CACHED_COVERS; i-- > 0;) {
+        auto songToInvalidate = manager.coverCacheInvalidator[i];
+        
+        // Skip selected level
+        if(manager.lastSelectedLevel == songToInvalidate.level)
+			continue;
+
+        manager.coverCacheInvalidator.erase(manager.coverCacheInvalidator.begin()+i);
+
+        if(songToInvalidate.level != nullptr && songToInvalidate.cover != nullptr && songToInvalidate.cover->m_CachedPtr.m_value != nullptr) {
+            auto song = reinterpret_cast<CustomPreviewBeatmapLevel*>(songToInvalidate.level);
+
+            song->coverImage = nullptr;
+            UnityEngine::Object::DestroyImmediate(songToInvalidate.cover->get_texture());
+            UnityEngine::Object::DestroyImmediate(songToInvalidate.cover);
+        }
+
+    }
+						
+
+    return System::Threading::Tasks::Task_1<UnityEngine::Sprite *>::FromResult(self->coverImage);
+    
+
+    // return CustomPreviewBeatmapLevel_GetCoverImageAsync(self, cancellationToken);
+    // postfix
+   
+};
+
 
 
 // Called at the early stages of game loading
@@ -200,12 +301,14 @@ extern "C" void load() {
     INSTALL_HOOK(getLoggerOld(), ReturnToBSS);
     INSTALL_HOOK(getLoggerOld(), GameplaySetupViewController_RefreshContent);
     INSTALL_HOOK(getLoggerOld(), LevelFilteringNavigationController_Setup);
+    INSTALL_HOOK(getLoggerOld(), StandardLevelDetailView_SetContent);
+    INSTALL_HOOK(getLoggerOld(), CustomPreviewBeatmapLevel_GetCoverImageAsync);
     // INSTALL_HOOK(getLoggerOld(), MainFlowCoordinator_DidActivate);
     
 
     custom_types::Register::AutoRegister();
 
     modInfo.id = MOD_ID;
-    
+
     manager.Init();
 }
