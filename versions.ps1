cb729fb275a0c0a8d749dce5f1fd7b2aedf860f9
Alex Uskov <FrozenAlex@users.noreply.github.com>
2023-09-16 11:48:35 +0400

    Add cool release scripts from Nya

---
+++ versions.ps1
@@ -0,0 +1,18 @@
+# File with versions of beat saber to build for mapped to branches
+# If releasing for new version of beat saber, add new version to the end of the list and add new branch to branches list
+
+# Branches to get version from
+$branches = @(
+    '1.25.1',
+    '1.26.2',
+    '1.27.0',
+    '1.28.0'
+)
+
+# Versions of beat saber 1:1 with tags
+$versions = @(
+    '1.25.1',
+    '1.26.2',
+    '1.27.0',
+    '1.28.0'
+)
