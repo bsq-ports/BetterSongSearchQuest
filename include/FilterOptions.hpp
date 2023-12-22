7f22f37b28a5b9569373d11f1f14bfef5b31548a
Alex Uskov <FrozenAlex@users.noreply.github.com>
2023-12-22 23:13:00 +0400

    Fix fmt

--- include/FilterOptions.hpp
+++ include/FilterOptions.hpp
@@ -72,7 +72,7 @@ public:
     const float STAR_FILTER_MAX = 15.0f;
     const float NJS_FILTER_MAX = 25.0f;
     const float NPS_FILTER_MAX = 12.0f;
-    static const int64_t BEATSAVER_EPOCH = 1525136400;
+    static inline const int64_t BEATSAVER_EPOCH = 1525136400;
 
     //General
     DownloadFilterType downloadType = DownloadFilterType::All;
