02293d1e3a65382b19a46bcff04445111ea3ff4d
Alex Uskov <FrozenAlex@users.noreply.github.com>
2025-04-09 03:50:11 +0400

    Fix list cells not being interactable on 1.40.4

--- include/UI/ViewControllers/SongListCellTableData.hpp
+++ include/UI/ViewControllers/SongListCellTableData.hpp
@@ -1,35 +1,32 @@
 #pragma once
 
-
-#include "UI/ViewControllers/SongListCell.hpp"
-#include "HMUI/Touchable.hpp"
-#include "HMUI/TableView.hpp"
-#include "bsml/shared/BSML.hpp"
-#include "TMPro/TextMeshProUGUI.hpp"
-#include "UnityEngine/UI/HorizontalOrVerticalLayoutGroup.hpp"
 #include "assets.hpp"
+#include "bsml/shared/BSML.hpp"
+#include "HMUI/TableView.hpp"
+#include "HMUI/Touchable.hpp"
+#include "TMPro/TextMeshProUGUI.hpp"
+#include "UI/ViewControllers/SongListCell.hpp"
+#include "UnityEngine/UI/HorizontalOrVerticalLayoutGroup.hpp"
 
-const StringW CustomSongListTableCellReuseIdentifier = "REUSECustomSongListTableCell";
+StringW const CustomSongListTableCellReuseIdentifier = "REUSECustomSongListTableCell";
 
-namespace BetterSongSearch::UI::ViewControllers
-{
-    class SongListTableData
-    {
-    public:
-        static CustomSongListTableCell *GetCell(HMUI::TableView *tableView)
-        {
+namespace BetterSongSearch::UI::ViewControllers {
+    class SongListTableData {
+       public:
+        static CustomSongListTableCell* GetCell(HMUI::TableView* tableView) {
             auto tableCell = tableView->DequeueReusableCellForIdentifier(CustomSongListTableCellReuseIdentifier);
-            if (!tableCell)
-            {
-                //  old tableCell = UnityEngine::GameObject::New_ctor("CustomSongListTableCell", csTypeOf(HMUI::Touchable *))->AddComponent<CustomSongListTableCell *>();
+            if (!tableCell) {
+                //  old tableCell = UnityEngine::GameObject::New_ctor("CustomSongListTableCell", csTypeOf(HMUI::Touchable
+                //  *))->AddComponent<CustomSongListTableCell *>();
 
-                tableCell = UnityEngine::GameObject::New_ctor("CustomSongListTableCell")->AddComponent<CustomSongListTableCell *>();
+                tableCell = UnityEngine::GameObject::New_ctor("CustomSongListTableCell")->AddComponent<CustomSongListTableCell*>();
+                tableCell->_groupsAllowInteraction = true;  // needed for the cell to be interactable in 1.40.4
                 tableCell->set_interactable(true);
                 tableCell->set_reuseIdentifier(CustomSongListTableCellReuseIdentifier);
                 BSML::parse_and_construct(Assets::SongListCell_bsml, tableCell->get_transform(), tableCell);
 
                 // Weird hack cause HMUI touchable is not there for some reason, thanks RedBrumbler
-                tableCell->get_gameObject()->AddComponent<HMUI::Touchable *>();
+                tableCell->get_gameObject()->AddComponent<HMUI::Touchable*>();
 
                 auto cell = tableCell.cast<CustomSongListTableCell>();
                 cell->diffs = cell->diffsContainer->GetComponentsInChildren<TMPro::TextMeshProUGUI*>();
@@ -38,4 +35,4 @@ namespace BetterSongSearch::UI::ViewControllers
             return tableCell.cast<CustomSongListTableCell>();
         }
     };
-}
+}  // namespace BetterSongSearch::UI::ViewControllers
