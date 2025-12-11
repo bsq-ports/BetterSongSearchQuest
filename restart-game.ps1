5883a076715d6e64b7a9aa0b327211c01ded5040
Alex Uskov <FrozenAlex@users.noreply.github.com>
2025-12-11 05:26:24 +0400

    Bump to 1.41.0_11623

--- restart-game.ps1
+++ restart-game.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
 adb shell am force-stop com.beatgames.beatsaber
-adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
+adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerGameActivity
 # Workaround for Meta piracy notification
 Start-Sleep -Seconds 1.0
-adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
+adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerGameActivity
